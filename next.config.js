9f81c83d78467c750fc16c5b3585cfb766c48827
Siddarth Jayakumar Natarajan <sid@Siddarths-MacBook-Air.local>
2026-03-08 15:24:07 +0800

    fix: add basePath for GitHub Pages and simplify workflow

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	basePath: "/personal_website",
 	output: "export",
 	distDir: "dist",
 	images: {
